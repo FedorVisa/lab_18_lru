b4925731882ada6134d9e9119ffbfe0ae42cdd57
FedorVisa <floodywizard@gmail.com>
2021-10-30 14:57:30 +0300

    osnovnoe zadanie

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "Lrucashe.h"
+
+ int main(){
+	 LRUcashe a(3);
+
+
+	 a.find_and_add1("AA");
+	 a.find_and_add1("AB");
+	 std::cout << a;
+	 std::cout << '\n';
+	 a.find_and_add1("AG");
+	 std::cout << a;
+	 std::cout << '\n';
+	 a.find_and_add1("AA");
+	 std::cout << a;
+	 std::cout << '\n';
+	 a.find_and_add1("AE");
+	 std::cout << a;
+	 std::cout << '\n';
+	 a.find_and_add1("AA");
+	 std::cout << a;
+
+	 return 0;
+}
