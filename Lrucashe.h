ba3dc72051f300e0e403ab92a8000db5f43f674c
FedorVisa <floodywizard@gmail.com>
2021-11-30 13:51:00 +0300

    dop sdan

--- Lrucashe.h
+++ Lrucashe.h
@@ -4,8 +4,7 @@
 #include<iostream>
 #include<map>
 #include<list>
-#include<string>
-#include<algorithm>
+
 
 using namespace std;
 
@@ -16,20 +15,66 @@ private:
 	size_t size;
 	size_t count;
 	list <T> qlist;
-	map <T, int> qmap;
+	map <T, decltype(qlist.begin())> qmap;
 public:
-	LRUcashe(size_t size1);
-	~LRUcashe();
-	void delete_check();
-	void add_to(const T& address);
-	void change_pos(const T& address);
-	bool find_and_add1(const T& address);
-	friend ostream& operator << (ostream& os, LRUcashe<T>& a);
+
+	LRUcashe(size_t size1) {
+		size = size1;
+		count = 0;
+	}
+
+	void delete_check() {
+		if (count == size) {
+			typename  list<T>::iterator it = qlist.end();
+			--it;
+			qmap.erase(qmap.find(*it));
+			qlist.pop_back();
+			count--;
+		}
+	}
+
+	void add_to(const T& address) {
+		qlist.push_front(address);
+		qmap.insert(pair<T,qlist.begin()>(address, qlist.begin()));
+	}
+
+	void change_pos(const T& address) {
+		typename list<T>::iterator it;
+		for (it = qlist.begin(); it != qlist.end(); ++it) {
+			if (*it == address) {
+				qlist.erase(it);
+				break;
+			}
+		}
+		qlist.push_front(address);
+	}
+
+	bool find_and_add1(const T& address) {
+		if (qmap.find(address) == qmap.end()) {
+			delete_check();
+			add_to(address);
+			count++;
+			return false;
+		}
+		else {
+			change_pos(address);
+		}
+		return true;
+	}
+
+	friend ostream& operator << (ostream& os, const LRUcashe<T>& a) {
+		for (T data : a.qlist) {
+			os << data << '\n';
+		}
+		return os;
+	}
 };
 
 
 
 #endif
+
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
  int main(){
 	 LRUcashe<std::string> a(3);
 
-
 	 a.find_and_add1("AA");
 	 a.find_and_add1("AB");
 	 std::cout << a;
@@ -19,6 +18,17 @@
 	 std::cout << '\n';
 	 a.find_and_add1("AA");
 	 std::cout << a;
+	 std::cout << '\n';
 
+
+	 LRUcashe<int> b(2);
+
+	 b.find_and_add1(1);
+	 b.find_and_add1(4);
+	 std::cout << b;
+	 std::cout << '\n';
+	 b.find_and_add1(7);
+	 std::cout << b;
+	 std::cout << '\n';
 	 return 0;
 }
